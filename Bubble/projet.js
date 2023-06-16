e9397012e7a76362c7f520c63a6eb6a2add4c318
Sadjoaldi <alsannealdi@outlook.com>
2023-06-16 04:17:53 +0200

    fin du bubble c'etait cool et fun

--- Bubble/projet.js
+++ Bubble/projet.js
@@ -1,3 +1,6 @@
+const counterDisplay = document.querySelector("h3");
+let counter = 0;
+
 const bubbleMaker = () => {
   // Créer un élément div
   const bubble = document.createElement("span");
@@ -13,9 +16,19 @@ const bubbleMaker = () => {
   bubble.style.top = Math.random() * 100 + 50 + "%";
   bubble.style.left = Math.random() * 100 + "%";
 
-  bubble.style.setProperty("--left", Math.random() * 100 + "%");
-  // bubble.style.borderRadius = "50%";
-  // bubble.style.backgroundColor = "blue";
+  const plusMinus = Math.random() > 0.5 ? 1 : -1;
+
+  bubble.style.setProperty("--left", Math.random() * 100 * plusMinus + "%");
+
+  bubble.addEventListener("click", () => {
+    counter++;
+    counterDisplay.textContent = counter;
+    bubble.remove();
+  });
+
+  setTimeout(() => {
+    bubble.remove();
+  }, 8000);
 };
 
-setInterval(bubbleMaker, 300);
+setInterval(bubbleMaker, 1000);
